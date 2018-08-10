210433efd84dd8a35e9f4b15cee18b44178dc4cb
A.Ageev <a.ageev@corepartners.com>
2018-08-10 14:34:02 +0300

    Add project files.

---
+++ SoundMutter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundMutter
+{
+	internal static class NativeMethods
+	{
+		[DllImport("kernel32.dll")]
+		internal static extern IntPtr GetConsoleWindow();
+	}
+
+	class Program
+	{
+		private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+		private const int WM_APPCOMMAND = 0x319;
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+		public static IntPtr Handle
+		{
+			get { return NativeMethods.GetConsoleWindow(); }
+		}
+
+		static void Main(string[] args)
+		{
+			SendMessageW(Handle, WM_APPCOMMAND, Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+			return;
+		}
+	}
+}
